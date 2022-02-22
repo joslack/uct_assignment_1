cc810e89e42c847b323a12920f4f03099afdb86c
joslack <jcsslack@gmail.com>
2022-02-22 13:12:47 +0200

    initialized project structure with a file for the main loop and files for parsing functionality

---
+++ driver.cpp
@@ -0,0 +1,8 @@
+#include "parser.h"
+
+int main(void)
+{
+  std::cout << "r: read and process input file.\np: print all tags.\nd: dump tags and data to file.\nl: list data for a specific tag.\nq: quit\npress r,p,d,l, or q followed by ENTER.";
+
+  return 0;
+}
