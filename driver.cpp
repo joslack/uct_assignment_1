9778d7ecdb1b70da9837437e075f8397cbdfbfba
joslack <jcsslack@gmail.com>
2022-02-23 15:29:00 +0200

    defined keywords cin, cout, clear, and TagStruct according to their namespace for clarity

--- driver.cpp
+++ driver.cpp
@@ -1,58 +1,69 @@
 #include "parser.h"
+#define cout std::cout
+#define cin std::cin
+#define clear SLCJON002::clear
+#define TagStruct SLCJON002::TagStruct
 
 int main(void)
 {
   bool breakflag = false;
   char input;
+  TagStruct tags;
   for (;;)
   {
     if (!input)
     {
-      std::cout << "r: read and process input file.\np: print all tags.\nd: dump tags and data to file.\nl: list data for a specific tag.\nq: quit\npress r,p,d,l, or q followed by ENTER: ";
-      std::cin >> input;
+      cout << "r: read and process input file.\np: print all tags.\nd: dump tags and data to file.\nl: list data for a specific tag.\nq: quit\npress r,p,d,l, or q followed by ENTER: ";
+      cin >> input;
     }
 
-    SLCJON002::clear();
+    clear();
 
     switch (input)
     {
+
     case 'r':
-      std::cout << "read file\n";
+      clear();
+      cout << "Please enter the name of the file to be parsed: " << std::endl;
       input = 0;
       break;
+
     case 'p':
-      std::cout << "print tags\n";
+      cout << "print tags\n";
       input = 0;
       break;
+
     case 'd':
-      std::cout << "dump tags\n";
+      cout << "dump tags\n";
       input = 0;
       break;
+
     case 'l':
-      std::cout << "list data for tag\n";
+      cout << "list data for tag\n";
       input = 0;
       break;
+
     case 'q':
       breakflag = true;
       break;
     };
     if (breakflag)
       break;
-    std::cout << "m: access the menu\nq: quit\npress m,q followed by ENTER: ";
-    std::cin >> input;
+
+    cout << "m: access the menu\nq: quit\npress m,q followed by ENTER: ";
+    cin >> input;
     switch (input)
     {
+
     case 'm':
       input = 0;
       break;
+
     case 'q':
       breakflag = true;
       break;
     };
-    SLCJON002::clear();
-
-    if (breakflag)
-      break;
+    clear() if (breakflag) break;
   }
   return 0;
 }
